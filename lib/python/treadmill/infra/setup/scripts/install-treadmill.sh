ddb401adf1f6bc6e9c46808a7113058ac568f16b
Brett Tegart <16308058+bretttegart@users.noreply.github.com>
2018-02-09 15:40:01 +0000

    Updating standup to pull from yum repository rather than build servers

--- lib/python/treadmill/infra/setup/scripts/install-treadmill.sh
+++ lib/python/treadmill/infra/setup/scripts/install-treadmill.sh
@@ -1,9 +1,2 @@
-# Install S6, execline and pid1
-yum install http://192.168.241.33/s6-2.6.2.0-1.el7.x86_64.rpm --nogpgcheck -y
-yum install http://192.168.241.33/execline-2.3.0.4-1.el7.x86_64.rpm --nogpgcheck -y
-yum install http://192.168.241.33/treadmill-pid1-1.0-3.x86_64.rpm --nogpgcheck -y
-
-# Install treadmill
-mkdir -p /opt/treadmill/bin
-curl -L http://192.168.241.33/treadmill -o /opt/treadmill/bin/treadmill
-chmod +x /opt/treadmill/bin/treadmill
+# Install Treadmill, S6, execline and pid1
+yum install -y s6 execline treadmill treadmill-pid1
