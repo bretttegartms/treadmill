b513a6e62228af9e026f4d9609ecc225e949ff55
Cesar Garcia <7408232+cesaragarcia@users.noreply.github.com>
2018-02-21 11:56:37 -0500

    provision host certs with ipa client install

--- lib/python/treadmill/infra/setup/scripts/install-ipa-client-with-otp.sh
+++ lib/python/treadmill/infra/setup/scripts/install-ipa-client-with-otp.sh
@@ -3,7 +3,8 @@ yum install -y ipa-client
 
 ipa-client-install --unattended --no-ntp \
     --mkhomedir --no-krb5-offline-password \
-    --password '{{ OTP }}' --enable-dns-updates
+    --password '{{ OTP }}' --enable-dns-updates \
+    --request-cert
 
 # /etc/krb5.conf
 # 
